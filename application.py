5dc518fa271874fc42cbacf18f6650f311dfc942
Nikita <thenromanov@gmail.com>
2020-03-24 21:55:31 +0300

    Added postal code checkbox

--- application.py
+++ application.py
@@ -17,58 +17,10 @@ class Example(QMainWindow):
         self.scale = 15
         self.type = 'map'
         self.point = []
+        self.address = ''
+        self.index = ''
         self.getImage()
         self.initUI()
-
-    def getImage(self):
-        mapServer = 'http://static-maps.yandex.ru/1.x/'
-        mapParams = {
-            'll': ','.join(map(str, self.coords)),
-            'z': str(self.scale),
-            'l': self.type
-        }
-        if len(self.point) > 0:
-            mapParams['pt'] = '{},{},comma'.format(self.point[0], self.point[1])
-        response = requests.get(mapServer, params=mapParams)
-
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        self.mapFile = "map.png"
-        with open(self.mapFile, "wb") as file:
-            file.write(response.content)
-            file.close()
-
-    def updateMap(self):
-        self.getImage()
-        self.pixmap = QPixmap(self.mapFile)
-        self.image.setPixmap(self.pixmap)
-        self.update()
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_PageUp:
-            self.scale = min(17, self.scale + 1)
-        elif event.key() == Qt.Key_PageDown:
-            self.scale = max(4, self.scale - 1)
-        elif event.key() == Qt.Key_Right:
-            self.coords[0] += 844 / 2 ** self.scale
-            if self.coords[0] > 180:
-                self.coords[0] -= 360
-        elif event.key() == Qt.Key_Left:
-            self.coords[0] -= 844 / 2 ** self.scale
-            if self.coords[0] < -180:
-                self.coords[0] += 360
-        elif event.key() == Qt.Key_Up:
-            self.coords[1] += 360 / 2 ** self.scale
-            if self.coords[1] > 90:
-                self.coords[1] -= 180
-        elif event.key() == Qt.Key_Down:
-            self.coords[1] -= 360 / 2 ** self.scale
-            if self.coords[1] < -90:
-                self.coords[1] += 180
-        self.updateMap()
 
     def initUI(self):
         self.setGeometry(100, 100, *screenSize)
@@ -110,6 +62,71 @@ class Example(QMainWindow):
 
         self.show()
 
+    def keyPressEvent(self, event):
+        print(event.key())
+        if event.key() == Qt.Key_PageUp:
+            self.scale = min(17, self.scale + 1)
+        elif event.key() == Qt.Key_PageDown:
+            self.scale = max(4, self.scale - 1)
+        elif event.key() == Qt.Key_Right:
+            self.coords[0] += 844 / 2 ** self.scale
+            if self.coords[0] > 180:
+                self.coords[0] -= 360
+        elif event.key() == Qt.Key_Left:
+            self.coords[0] -= 844 / 2 ** self.scale
+            if self.coords[0] < -180:
+                self.coords[0] += 360
+        elif event.key() == Qt.Key_Up:
+            self.coords[1] += 360 / 2 ** self.scale
+            if self.coords[1] > 90:
+                self.coords[1] -= 180
+        elif event.key() == Qt.Key_Down:
+            self.coords[1] -= 360 / 2 ** self.scale
+            if self.coords[1] < -90:
+                self.coords[1] += 180
+        self.updateMap()
+
+    def mousePressEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            deltaCoords = [event.x() - screenSize[0] / 2, event.y() - screenSize[1] / 2]
+            self.point = [self.coords[0] + deltaCoords[0] * 844 / 600 / 2 ** self.scale,
+                          self.coords[1] - (deltaCoords[1] + 15) * 360 / 450 / 2 ** self.scale]
+            data = getAddresses(','.join(map(str, self.point)))
+            self.address = getFullAddress(data)
+            self.index = getPostalCode(data)
+            text = self.address
+            if self.checkBox.isChecked() and self.index:
+                text += ', ' + self.index
+            self.text.setText(text)
+            self.updateMap()
+
+    def getImage(self):
+        mapServer = 'http://static-maps.yandex.ru/1.x/'
+        mapParams = {
+            'll': ','.join(map(str, self.coords)),
+            'z': str(self.scale),
+            'l': self.type
+        }
+        if len(self.point) > 0:
+            mapParams['pt'] = '{},{},comma'.format(self.point[0], self.point[1])
+        response = requests.get(mapServer, params=mapParams)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        self.mapFile = "map.png"
+        with open(self.mapFile, "wb") as file:
+            file.write(response.content)
+            file.close()
+
+    def updateMap(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.mapFile)
+        self.image.setPixmap(self.pixmap)
+        self.update()
+
     def search(self):
         address, okBtnPressed = QInputDialog.getText(self, 'Введите адрес', 'Введите адрес')
         if okBtnPressed:
@@ -118,8 +135,20 @@ class Example(QMainWindow):
             if coords:
                 self.coords = coords[0].copy()
                 self.point = coords[0].copy()
-                self.text.setText(getFullAddress(data))
+                self.address = getFullAddress(data)
+                self.index = getPostalCode(data)
+                text = self.address
+                if self.checkBox.isChecked() and self.index:
+                    text += ', ' + self.index
+                self.text.setText(text)
                 self.updateMap()
+
+    def changeText(self, state):
+        if self.text.text() != '':
+            if state == Qt.Checked and self.index:
+                self.text.setText(self.address + ', ' + self.index)
+            else:
+                self.text.setText(self.address)
 
     def clear(self):
         self.point = []
